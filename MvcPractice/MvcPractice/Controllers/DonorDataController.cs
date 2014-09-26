e8154c583e5e5c25df65f2e15518bc1ea2835900
SeedPaths-User <lessdismissive@email.com>
2014-09-26 16:33:09 -0600

    Crudin it up

--- MvcPractice/MvcPractice/Controllers/DonorDataController.cs
+++ MvcPractice/MvcPractice/Controllers/DonorDataController.cs
@@ -8,13 +8,43 @@ namespace MvcPractice.Controllers
 {
     public class DonorDataController : Controller
     {
+        Models.Blood_BankEntities db = new Models.Blood_BankEntities();
         //
         // GET: /DonorData/
-
         public ActionResult Index()
         {
-            return View();
+            return View(db.Donors);
         }
 
+        public ActionResult Details(int id)
+        {
+            return View(db.Donors.Find(id));
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.Donors.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Models.Donor donor)
+        {
+            db.Entry(donor).State = System.Data.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index", "DonorData");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.Donors.Find(id));
+        }
+
+        public ActionResult DeleteConfirm(int id)
+        {
+            Models.Donor formToDelete = db.Donors.Find(id);
+            db.Donors.Remove(formToDelete);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "DonorData");
+        }
     }
 }
--- MvcPractice/MvcPractice/Controllers/PatientDataController.cs
+++ MvcPractice/MvcPractice/Controllers/PatientDataController.cs
@@ -8,13 +8,44 @@ namespace MvcPractice.Controllers
 {
     public class PatientDataController : Controller
     {
+        Models.Blood_BankEntities db = new Models.Blood_BankEntities();
         //
         // GET: /PatientData/
 
         public ActionResult Index()
         {
-            return View();
+            return View(db.Patients);
         }
 
+        public ActionResult Details(int id)
+        {
+            return View(db.Patients.Find(id));
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.Patients.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Models.Patient patient)
+        {
+            db.Entry(patient).State = System.Data.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index", "PatientData");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.Patients.Find(id));
+        }
+
+        public ActionResult DeleteConfirm(int id)
+        {
+            Models.Patient formToDelete = db.Patients.Find(id);
+            db.Patients.Remove(formToDelete);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "PatientData");
+        }
     }
 }
